375b0c9ff78fff468190e0f38b4a5bbfded4912b
jaezuchowski <jess.zuchowski@gmail.com>
2019-04-21 12:55:07 -0700

    added two more test arrays

--- challenges/ArrayReverse.java
+++ challenges/ArrayReverse.java
@@ -2,8 +2,13 @@ import java.util.Arrays;
 
 public class ArrayReverse {
   public static void main(String[] args) {
+    //3 test arrays
     int[] originalArray = {
       1, 2, 3, 4, 5, 6};
+    // int[] originalArray = {
+    //   2, 4, 6, 8};
+    // int[] originalArray = {
+    //   7, 8, 9, 10, 11, 12, 13};
       reverseArray(originalArray);
   }
   public static int[] reverseArray(int[] originalArray) {
@@ -13,7 +18,7 @@ public class ArrayReverse {
       originalArray[i] = originalArray[originalArray.length - i - 1];
       originalArray[originalArray.length - i - 1] = temp;
     }
-    System.out.println(originalArray);
+    System.out.println(Arrays.toString(originalArray));
     return originalArray;
   }
 }
